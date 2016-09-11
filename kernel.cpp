0aa0b4789583cc6cba00b3c1632b74828d3677f9
Le0nX <cloudmindone@gmail.com>
2016-09-11 17:47:51 +0000

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,6 @@
-int main(){
+void kernelMain(){
     printf("Welcome to UNI OS v.0.1\n Copyright by Nefedov & Serkov. Moscow 2016.");
     
-    while(1); //inf loop
+    while(1); //inf loop. Ядро же не может остановиться, лол
     
-    return 0;
     }
