f8b31d44c3c72ef21ad8d36b595c9f781886bf20
Le0nX <cloudmindone@gmail.com>
2016-09-12 16:01:42 +0300

    some changes

--- loader.s
+++ loader.s
@@ -1,13 +1,13 @@
-.section .text      ;секция кода программы
-.extern kernelMain  ;объявляем функцию из другого файла(kernel.cpp)
-.global loader      ;глобальная метка. Точка входа в наш загрузчик
+.section .text      #секция кода программы
+.extern kernelMain  #объявляем функцию из другого файла(kernel.cpp)
+.global loader      #глобальная метка. Точка входа в наш загрузчик
 
 loader:
-    mov $kernel_stack, %esp ;stack pointer указывающий на стек ядра.
+    mov $kernel_stack, %esp #stack pointer указывающий на стек ядра.
     
     
     
     
-.section .bss       ;Доп. секция
-
+.section .bss       #Доп. секция
+.space 2*1024*1024 	#выделяем 2мб
 kernel_stack:
