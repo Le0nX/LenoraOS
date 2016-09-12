1c0c2e66d46ed21d4426618bcc0ccfc05f34fc3e
Le0nX <cloudmindone@gmail.com>
2016-09-12 17:10:54 +0300

    Dobavil stack i inf loop

--- loader.s
+++ loader.s
@@ -4,10 +4,16 @@
 
 loader:
     mov $kernel_stack, %esp #stack pointer указывающий на стек ядра.
+    call kernelMain			#вызов функции
     
-    
+ _stop:				#новая метка для inf loop, сраховка после inf loop в kernelMain
+ 	cli				#запрещаем прерывания от всех внешних устройств. Чистим регистр флагов
+ 	hlt				#стопит камень
+ 	jmp _stop
+
     
     
 .section .bss       #Доп. секция
 .space 2*1024*1024 	#выделяем 2мб
+
 kernel_stack:
