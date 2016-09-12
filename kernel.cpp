49d93b83a5fac85b53db3db1feff0f6ce2a702a1
Le0nX <cloudmindone@gmail.com>
2016-09-12 19:37:12 +0300

    printf func added

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,9 @@
+void printf(char* str){
+	(unsigned short*)VideoMemory = (unsigned short*)0xb8000;
+	for (int i =0; str[i] != '\0'; ++i)
+		VideoMemory[i] = str[i];
+}
+
 void kernelMain(void* multiboot_structure, unsigned int magicnumber){
     printf("Welcome to UNI OS v.0.1\n Copyright by Nefedov & Serkov. Moscow 2016.");
     
