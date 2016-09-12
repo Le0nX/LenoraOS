f5b4cd62be03eb1d5080768271ea4d70487053c7
Le0nX <cloudmindone@gmail.com>
2016-09-12 17:50:01 +0300

    Updated loader.s

--- loader.s
+++ loader.s
@@ -1,3 +1,14 @@
+.set MAGICNUM, 0x1badb002 #создали MAGICNUM со значением магического числа, чтобы GRUB распознал наше ядро.
+.set ALIGN, 1<<0 		  #выравнивание по границам
+.set MEMINFO, 1<<1 		  #карта памяти 
+.set CHECKSUM, -(MAGICNUM + FLAGS) #контрольная сумма, чтобы понять, что мы в multiboot
+
+.section .multiboot
+	.align 4
+	.long MAGIC
+	.long FLAGS
+	.long CHECKSUM
+
 .section .text      #секция кода программы
 .extern kernelMain  #объявляем функцию из другого файла(kernel.cpp)
 .global loader      #глобальная метка. Точка входа в наш загрузчик
