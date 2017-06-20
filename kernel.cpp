0405f2ab2a7638455d1ac15c09c7c68d13de6095
Le0nX <cloudmindone@gmail.com>
2017-06-20 05:39:28 +0300

    [feature] add own printf() with Black Jack & VideoMemmory

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,12 @@
+void printf(char *string)
+{
+	unsigned short* VideoMem = (unsigned short*)0xb8000;
+	
+	for (int i=0; str[i] != '\0'; ++i){
+		VideoMem[i] = (VideoMem[i] & 0xFF00) | str[i];
+	}
+}
+
 extern "C" void main(void *multiboot_struct, unsigned int MAGIC)
 {
 	printf("Welcome to Lenora OS...");
