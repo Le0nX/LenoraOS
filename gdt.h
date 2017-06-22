fd27aeec97858264dc9241874f3eed68bb9b9213
Le0nX <cloudmindone@gmail.com>
2017-06-22 10:47:03 +0300

    [fix] fixed some stuff in header

--- gdt.h
+++ gdt.h
@@ -1,6 +1,7 @@
 #ifndef __GDT_H
 #define __GDT_H
 
+#include "types.h"
 
 	class GlobalDescriptorTable
 	{
@@ -14,10 +15,10 @@
 		   * что в этой структуре выравнивание не выполняется.
 		   */
 			  private:
-				  uint16_t LimitLow;		// Младшие 16 битов граничного значения limit.
-				  uint16_t BaseLow;			// Младшие 16 битов адресной базы.
-				  uint8_t  BaseMiddle;		// Следующие 8 битов адресной базы.
-				  uint8_t  Access;			// Флаги доступа, определяющие в каком кольце можно использовать этот сегмент.
+				  uint16_t LimitLow;        // Младшие 16 битов граничного значения limit.
+				  uint16_t BaseLow;         // Младшие 16 битов адресной базы.
+				  uint8_t  BaseMiddle;      // Следующие 8 битов адресной базы.
+				  uint8_t  Access;          // Флаги доступа, определяющие в каком кольце можно использовать этот сегмент.
 				  uint8_t  flags_limit_hi;  // Флаги 4бит и старшие 4бит limit
 				  uint8_t  BaseHigh;		// Последние 8 битов адресной базы.
 			  public:
@@ -25,7 +26,7 @@
 				  uint32_t Base();
 				  uint32_t Limit(); 
 				  
-		  } __attribute__((packed));		/* отключаем выравнивание */
+		  } __attribute__((packed));        /* отключаем выравнивание */
 	  
 	  SegmentDescriptor nullSegmentSelector;
 	  SegmentDescriptor unusedSegmentSelector;
@@ -35,8 +36,8 @@
 	  GlobalDescriptorTable();
 	  ~GlobalDescriptorTable();
 	  
-	  uint16_t codeSegmentSelector(); 		//возвращает смещение дескриптора сегмента кода
-	  uint16_t dataSegmentSelector(); 		//возвращает смещение дескриптора сегмента данных  
+	  uint16_t CodeSegmentSelector(); 		//возвращает смещение дескриптора сегмента кода
+	  uint16_t DataSegmentSelector(); 		//возвращает смещение дескриптора сегмента данных  
 	    
 	};
 	
