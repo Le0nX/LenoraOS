7ddd91e2ca72b4c506eb837a968c1096a49423c3
Le0nX <cloudmindone@gmail.com>
2017-06-24 14:22:33 +0300

    [feature] add support of interrupts to kernel

--- interrupt.h
+++ interrupt.h
@@ -8,32 +8,53 @@
   class InterruptManager
   {
 	  protected:
+	  // Структура, описывающая шлюз прерываний.
 			struct GateDescriptor
 			{
-				uint16_t    handlerAddressLowBits;
-				uint16_t    gtd_codeSegmentSelector;
-				uint8_t     reserved;
-				uint8_t     access;
-				uint16_t    handlerAddressHighBits;
+				uint16_t    handlerAddressLowBits;    // Младшие 16 битов адреса, куда происходи переход в случае возникновения прерывания.
+				uint16_t    gdt_codeSegmentSellector;  // Переключатель сегмента ядра.
+				uint8_t     reserved;                 // Это значение всегда должно быть нулевым.
+				uint8_t     access;                   // Флаги
+				uint16_t    handlerAddressHighBits;   // Старшие 16 битов адреса, куда происходи переход.
 				
-			} __atribute__((packed));
+			} __attribute__((packed));
 			
 			static GateDescriptor interruptDescriptorTable[256];
 			
+			
+			struct InterruptDescriptorTablePointer
+			{
+				uint16_t size;
+				uint32_t base;
+			} __attribute__((packed));
+			
+			
 			static void SetInterruptDescriptorTableEntry(
 						uint8_t interruptNumber,
-						uint16_t gdt_codeSegmentSellectorOffset,
+						uint16_t codeSegmentSellectorOffset,
 						void (*handler)(),
 						uint8_t DescriptorPrivilegeLevel,
 						uint8_t DescriptorType
 			);
 			
+			
+			Port8bitSlow picMasterComand;
+			Port8bitSlow picMasterData;
+			
+			Port8bitSlow picSlaveComand;
+			Port8bitSlow picSlaveData;
+			
+			
+			
 	  public:
 			InterruptManager(GlobalDescriptorTable* gdt);
 			~InterruptManager();
 			
+			void Activate();
+			
 			static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
 			
+// Эти внешние директивы позволят нам получить доступ к адресам наших ассемблерных обработчиков прерываний ISR.			
 			static void 	IgnoreInterruptRequest();
 			static void     HandleInterruptRequest0x00();
 			static void     HandleInterruptRequest0x01();
