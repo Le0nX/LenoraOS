50446ef64a781c48f503f57dda514d5c0a68ca19
Dmitry Serkov <komochek32@ya.ru>
2017-06-27 21:36:14 +0300

    Added all chars support. You can write!

--- keyboard.cpp
+++ keyboard.cpp
@@ -47,18 +47,46 @@ uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
 		
 		
 		//блок для первой строки q-]
-		case 0x10: printf("q"); break;
-		case 0x11: printf("w"); break;
+		case 0x10: if (shift) printf("Q"); else printf("q"); break;
+		case 0x11: if (shift) printf("W"); else printf("w"); break;
+		case 0x12: if (shift) printf("E"); else printf("e"); break;
+		case 0x13: if (shift) printf("R"); else printf("r"); break;
+		case 0x14: if (shift) printf("T"); else printf("t"); break;
+		case 0x15: if (shift) printf("Y"); else printf("y"); break;
+		case 0x16: if (shift) printf("U"); else printf("u"); break;
+		case 0x17: if (shift) printf("I"); else printf("i"); break;
+		case 0x18: if (shift) printf("O"); else printf("o"); break;
+		case 0x19: if (shift) printf("P"); else printf("p"); break;
+		case 0x1A: if (shift) printf("{"); else printf("["); break;
+		case 0x1B: if (shift) printf("}"); else printf("]"); break;
 		
 		
 		//блок для второй строки a-\n
-		case 0x1E: printf("a"); break;
-		case 0x1F: printf("s"); break;
-		case 0x20: printf("d"); break;
+		case 0x1E: if (shift) printf("A"); else printf("a"); break;
+		case 0x1F: if (shift) printf("S"); else printf("s"); break;
+		case 0x20: if (shift) printf("D"); else printf("d"); break;
+		case 0x21: if (shift) printf("F"); else printf("f"); break;
+		case 0x22: if (shift) printf("G"); else printf("g"); break;
+		case 0x23: if (shift) printf("H"); else printf("h"); break;
+		case 0x24: if (shift) printf("J"); else printf("j"); break;
+		case 0x25: if (shift) printf("K"); else printf("k"); break;
+		case 0x26: if (shift) printf("L"); else printf("l"); break;
+		case 0x27: if (shift) printf(":"); else printf(";"); break;
+		case 0x28: if (shift) printf("\""); else printf("'"); break;
 		
-		
+
 		//блок для третьей строки z-/
-		case 0x2C: printf("z"); break;
+		case 0x2C: if (shift) printf("Z"); else printf("z"); break;
+		case 0x2D: if (shift) printf("X"); else printf("x"); break;
+		case 0x2E: if (shift) printf("C"); else printf("c"); break;
+		case 0x2F: if (shift) printf("V"); else printf("v"); break;
+		case 0x30: if (shift) printf("B"); else printf("b"); break;
+		case 0x31: if (shift) printf("N"); else printf("n"); break;
+		case 0x32: if (shift) printf("M"); else printf("m"); break;
+		case 0x33: if (shift) printf("<"); else printf(","); break;
+		case 0x34: if (shift) printf(">"); else printf("."); break;
+		case 0x35: if (shift) printf("?"); else printf("/"); break;
+
 		
 		//stuff
 		case 0x1C: printf("\n"); break;
