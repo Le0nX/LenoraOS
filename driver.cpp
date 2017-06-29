7e0b845ac9b496dab0312335a95e62d3fd1c3305
Le0nX <cloudmindone@gmail.com>
2017-06-29 23:34:13 +0300

    [fix] fixed driver abstraction

--- driver.cpp
+++ driver.cpp
@@ -1,1 +1,37 @@
 #include "driver.h"
+
+Driver::Driver()
+{
+}
+
+
+Driver::~Driver()
+{}
+
+void Driver::Activate()
+{
+	
+}
+
+int Driver::Reset()
+{
+	
+}
+
+void Driver::Deatcivate()
+{
+	
+}
+
+
+DriverManager::DriverManager()
+{
+	
+}
+
+void DriverManager::AddDriver(Driver*)
+{
+	
+}
+
+
--- driver.h
+++ driver.h
@@ -3,6 +3,27 @@
 #define __DRIVER_H
 
 
+  class Driver
+  {
+	  public:
+		Driver();
+		~Driver();
+		
+		virtual void Activate();
+		virtual int Reset();
+		virtual void Deatcivate;  
+  };
 
 
+  class DriverManager
+  {
+	  private:
+		Driver* drivers[255];
+		int numDrivers;
+	  public:
+		DriverManager();
+		void AddDriver(Driver*);
+		
+		
+  };
 #endif
