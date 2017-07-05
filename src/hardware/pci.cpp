9f9337b6391c6f8c8c03f620ba998ade767e85cc
Le0nX <cloudmindone@gmail.com>
2017-07-05 20:54:37 +0300

    [feature] add some stuff to pci driver

--- src/hardware/pci.cpp
+++ src/hardware/pci.cpp
@@ -17,12 +17,26 @@ PCIcontroller::~PCIcontroller()
 uint32_t PCIcontroller::Read(uint16_t bus, uint16_t device, 
 							 uint16_t function, uint32_t offset)
 {
+	uint32_t id = 
+	0x1 << 31
+		| ((bus & 0xFF) << 16)
+		| ((device & 0x1F) << 11)
+		| ((function & 0x07) << 8)
+		| (offset & 0xFC);           // отрезаем последние 2bit
 	
+	commandPort.Write(id);
+	uint32_t result = dataPort.Read();
+		
 }
 
 
 void PCIcontroller::Write(uint16_t bus, uint16_t device, cuint16_t function, 
 					                    uint32_t offset, uint32_t value)
 {
-	
+	uint32_t id = 
+	0x1 << 31
+		| ((bus & 0xFF) << 16)
+		| ((device & 0x1F) << 11)
+		| ((function & 0x07) << 8)
+		| (offset & 0xFC);
 }
